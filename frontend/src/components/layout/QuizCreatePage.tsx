dfc64c84bc4b4dcdde1300934c98b66add431e74
Louis Charpentier <95610858+Clovelahuiss@users.noreply.github.com>
2025-11-13 17:01:02 +0100

    chore: setup development environment

---
+++ frontend/src/components/layout/QuizCreatePage.tsx
@@ -0,0 +1,8 @@
+// src/pages/QuizCreatePage.tsx
+import React from 'react';
+
+const QuizCreatePage: React.FC = () => {
+  return <div>Créer un quiz pour un cours</div>;
+};
+
+export default QuizCreatePage;
---
+++ frontend/src/components/layout/QuizPlayPage.tsx
@@ -0,0 +1,8 @@
+// src/pages/QuizPlayPage.tsx
+import React from 'react';
+
+const QuizPlayPage: React.FC = () => {
+  return <div>Jouer le quiz (affichage questions / réponses)</div>;
+};
+
+export default QuizPlayPage;
